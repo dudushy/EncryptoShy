ce0050774aae0957184708509eda2b4882ad1057
Natzun <natanzun@outlook.com.br>
2022-08-01 23:16:40 -0300

    Renomeando botao1
    Ae

--- EncryptoShy/Form1.Designer.cs
+++ EncryptoShy/Form1.Designer.cs
@@ -44,11 +44,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(452, 86);
+            this.button1.Location = new System.Drawing.Point(341, 81);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(233, 134);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Somando";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
